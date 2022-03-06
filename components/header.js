0897c0cc4d2a85a9f6cd3c91656da8c6dbbb502f
Vladislav <43251088+s7kind@users.noreply.github.com>
2022-03-06 14:17:15 +0200

    finish first blocks

--- components/header.js
+++ components/header.js
@@ -1,7 +1,14 @@
+import styles from '../styles/header.module.scss'
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className="header">
-
+        <div className={styles.header}>
+            <div className="container">
+                <Link href="/">
+                    <a><img className={styles.header__logo} src="/static/logo.svg" alt="Help Ukraine Online"/></a>
+                </Link>
+            </div>
         </div>
     )
 }
