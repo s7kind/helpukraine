f21e3d510459ac99d16a9c33064a6fc75bee069b
Vladislav <43251088+s7kind@users.noreply.github.com>
2022-03-06 19:27:49 +0200

    add example page

---
+++ pages/petition.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Petition() {
+    return (
+        <>
+            <Head>
+                <title>Help Ukraine Online - Petition</title>
+            </Head>
+
+            <main>
+                Petition page Example
+            </main>
+        </>
+    )
+}
