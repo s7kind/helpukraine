bb70dcad22e80bdc094914fae3b7b0b8a19e8f15
Vladislav <43251088+s7kind@users.noreply.github.com>
2022-03-07 17:39:11 +0200

    add animatation add start prepare better structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,24 @@
+import {ParallaxProvider} from 'react-scroll-parallax';
+
 import '../styles/app.scss'
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Head from 'next/head'
 
-function MyApp({Component, pageProps}) {
-    return (
-        <div className="main__app">
-            <Head>
-                <link rel="icon" href="/favicon.png"/>
-            </Head>
-            <Header/>
-            <Component {...pageProps} />
-            <Footer/>
-        </div>
 
+function MyApp({Component, pageProps}) {
+
+    return (
+        <ParallaxProvider>
+            <div className="main__app" data-scroll-section>
+                <Head>
+                    <link rel="icon" href="/favicon.png"/>
+                </Head>
+                <Header/>
+                <Component {...pageProps} />
+                <Footer/>
+            </div>
+        </ParallaxProvider>
     )
 }
 
