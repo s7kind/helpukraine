3e990afec2fda05249e7818e234287f1cdd249cd
Vladislav <43251088+s7kind@users.noreply.github.com>
2022-03-09 11:50:04 +0200

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,28 @@ function MyApp({Component, pageProps}) {
 
     return (
         <ParallaxProvider>
-            <div className="main__app" data-scroll-section>
+            <div className="main__app">
                 <Head>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                    })(window,document,'script','dataLayer','GTM-NT3J9L9');
+                        `
+                        }}>
+
+                    </script>
+                    <title>Help Ukraine Online</title>
                     <link rel="icon" href="/favicon.png"/>
                 </Head>
+                <noscript dangerouslySetInnerHTML={{
+                    __html: `
+                            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NT3J9L9" height="0" width="0" style="display:none; visibility:hidden"/>
+                            `
+                }}/>
                 <Header/>
                 <Component {...pageProps} />
                 <Footer/>
