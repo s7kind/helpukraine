486b397a9cea40ac04ab31b059d70340fabaf2e6
VitaliiKondratenko <vetal6430@gmail.com>
2022-03-22 04:39:16 +0200

    video popup layout

--- components/Home Page/hero.js
+++ components/Home Page/hero.js
@@ -1,10 +1,18 @@
 import {ParallaxBanner} from "react-scroll-parallax";
 import styles from "../../styles/main-page.module.scss";
 import Link from "next/link";
-import React from "react";
-
+import {useState} from "react";
+import action from "../../styles/main-page.module.scss";
 
 export default function Hero() {
+
+    const [popup, setPopup] = useState(false);
+
+    const handlePopup = (event) => {
+        event.preventDefault();
+        setPopup(!popup)
+    }
+
     return (
         <>
             {/* HERO */}
@@ -42,11 +50,37 @@ export default function Hero() {
                                     <span>Participate online</span>
                                 </a>
                             </Link>
+                            <div className={styles.banner__videoBtn}>
+                                <a href="" onClick={(event) => handlePopup(event)} class="button button__blue"> <span>Watch video</span></a>
+                            </div>
                         </div>
 
                     </div>
                 </div>
             </ParallaxBanner>
+            <div className={`popup ${popup === true ? 'active' : ''}`} style={{zIndex: '156'}}>
+                <div className="popup__wrap">
+                    <div className="popup__close" onClick={(event) => handlePopup(event)}>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 6L26 26" stroke="#9E9E9E" strokeWidth="2" strokeLinecap="round"/>
+                            <path d="M26 6L6 26" stroke="#9E9E9E" strokeWidth="2" strokeLinecap="round"/>
+                        </svg>
+                    </div>
+                    <div className="popup__info">
+                        <div className={action.take__action_info}>
+                            <div className={action.take__action_info_box}>
+                                <h2 className={action.take__action_title} style={{textAlign: 'center'}}>Video</h2>
+                                <p className="popup-msg" style={{lineHeight: '1.5'}}>
+
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
             {/* END HERO */}
         </>
     )
