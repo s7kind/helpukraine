9c629ad4b05ce2f02acb23fa1d9e56f35f3702b4
VitaliiKondratenko <vetal6430@gmail.com>
2022-04-06 22:06:15 +0300

    adding 18+ popup to russian crimes page

--- pages/russian-war-crimes.js
+++ pages/russian-war-crimes.js
@@ -1,16 +1,27 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/take-action.module.scss'
-import React, { useState, useCallback } from "react";
+import React, {useState, useCallback} from "react";
 import Gallery from "react-photo-gallery-next"
-import Carousel, { Modal, ModalGateway } from "react-images"
+import Carousel, {Modal, ModalGateway} from "react-images"
+import action from "../styles/main-page.module.scss";
 
-export default function RussianWarCrimes(){
+
+export default function RussianWarCrimes() {
+
+    const [popup, setPopup] = useState(true);
+    const [text, setMsg] = useState('');
+
+    const handlePopup = (event, msg) => {
+        event.preventDefault();
+        setPopup(!popup);
+        setMsg(msg);
+    }
 
     const [currentImage, setCurrentImage] = useState(0);
     const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
-    const openLightbox = useCallback((event, { photo, index }) => {
+    const openLightbox = useCallback((event, {photo, index}) => {
         setCurrentImage(index);
         setViewerIsOpen(true);
     }, []);
@@ -88,7 +99,8 @@ export default function RussianWarCrimes(){
                 <section className={styles.main__header_action}>
                     <div className={styles.container}>
                         <h1>Russians ARE war criminals!</h1>
-                        <p class={styles.container__title_descr}>Here is Evidence of Russian War Crimes Committed in Ukraine!</p>
+                        <p class={styles.container__title_descr}>Here is Evidence of Russian War Crimes Committed in
+                            Ukraine!</p>
                     </div>
                 </section>
                 <section className={styles.main__section_act}>
@@ -112,6 +124,43 @@ export default function RussianWarCrimes(){
                     </ModalGateway>
                 </section>
             </main>
+
+            <div className={`popup ${popup === true ? 'active' : ''}`} style={{zIndex: '156'}}>
+                <div className="popup__wrap">
+                    <div className="popup__close" onClick={(event) => handlePopup(event)}>
+                        <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 6L26 26" stroke="#9E9E9E" strokeWidth="2" strokeLinecap="round"/>
+                            <path d="M26 6L6 26" stroke="#9E9E9E" strokeWidth="2" strokeLinecap="round"/>
+                        </svg>
+                    </div>
+                    <img src="/crimes/18.png" style={{width: "55px",position: "absolute",top: "10px",left: "45%"}}/>
+
+                    <div className="popup__info">
+                        <div className={action.take__action_info}>
+                            <div className={action.take__action_info_box} style={{textAlign: "center"}}>
+                                <h2 className={action.take__action_title}>Warning! Severely
+                                    distressing content!</h2>
+                                <p className="popup-msg" style={{lineHeight: '1.5'}}>We do not recommend those under 18
+                                    years of age to view these photos. There are no adequate words to describe what took
+                                    place.</p>
+                                <div className={action.take__action_buttons}>
+                                    <button onClick={(event) => handlePopup(event)} className="button button__blue"
+                                       style={{cursor: 'pointer', marginRight: "30px"}}>
+                                        <span>Okey</span>
+                                    </button>
+                                    <a href="/" className="button button__blue">
+                                        <span>Return to Homepage</span>
+                                    </a>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
 
         </>
     )
